983c9adbf1a5f0926ae859d73eb8022141d9e4e4
gendi93 <yehia.gendi@gmail.com>
2017-07-23 21:29:30 +0100

    Create room skeleton
        - build walls
        - build door
        - write initial door script

--- 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright gendi93
 
 #include "PositionReport.h"
+#include <Gameframework/Actor.h>
 
 
 // Sets default values for this component's properties
@@ -18,8 +19,10 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	
 }
 
