9476a252f13fa6c8221c77b884d775bbef3ee8da
gendi93 <yehia.gendi@gmail.com>
2017-07-25 22:39:02 +0100

    Update room     - add colliders to doors     - add physics bodies to interactive objects     - update lighting

--- 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -41,12 +41,11 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		OUT PlayerViewPointLocation, 
 		OUT PlayerViewPointRotation);
 
-	// TODO Log out to test
 	/*UE_LOG(LogTemp, Warning, TEXT("Position: %s, Rotation: %s"),
 		*PlayerViewPointLocation.ToString(),
 		*PlayerViewPointRotation.ToString());*/
 
-	// Ray-cast out to reach distance
+	// /draw a red trace in the world to visualise
 	FVector LineTraceEnd = PlayerViewPointLocation
 		+ PlayerViewPointRotation.Vector()*Reach;
 	DrawDebugLine(
@@ -60,6 +59,24 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		10.f
 	);
 
+	// Setup query parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	// Line-trace (AKA ray-cast) out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
 	// See what we hit
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s has been hit"), *(ActorHit->GetName()));
+	}
 }
 
